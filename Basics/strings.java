bdf5698b8319b6f697f59ed52d8dfabb23421d4f
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2023-12-02 16:15:37 +0530

    8. Strings

---
+++ Basics/strings.java
@@ -0,0 +1,15 @@
+//What is a String?
+//String is a sequence of characters
+String a = "Hello World";
+//Also to execute multiple lines in JShell use { and press enter and to end the program use } and press enter
+//String concatenation
+//When you use the + sign it helps to concatenate two strings
+//Also note that strings are immune to everything
+//Example:
+int myInt = 10;
+String myString = "Hello";
+System.out.println(myString + myInt);
+//So instead of giving an error it will print Hello10
+//So what the string does it converts integer 10 into string 10
+//And then joins both the strings as it has a + sign
+//So it concatenates both
