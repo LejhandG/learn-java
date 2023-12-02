e7a05575ffd2d9b785e21089653632628bcab434
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2023-12-02 12:01:45 +0530

    3. Expressions

---
+++ Basics/expressions.java
@@ -0,0 +1,17 @@
+//An expression is the code segment which is on the right side of the equals sign
+//Example: 
+int a = 10;
+//So in this code 10 is the expression
+int b = (10*1) + (5*2);
+//So in this code (10*1) + (5*2) is the expression
+int myFirstNumber = 35;
+int mySecondNumber = 12;
+int myThirdNumber = 6;
+int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+//So now 35+12+6 would be stored in myTotal
+myThirdNumber = myFirstNumber * 2;
+//So now myThirdNumber is overwritten to 70
+myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+//So now the myTotal is 117
+//PLEASE NOT JAVA IS CASE SENSITIVE SO THE VARIABLE NAME SHOULD BE SAME AS THE DECLARED
+//Now we move to the next topic
