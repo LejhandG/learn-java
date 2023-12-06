b1ed1acf92ef2bb55b7fb55459e0fffbe46eebca
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2023-12-06 10:02:31 +0530

    1. Hello World in an IDE

---
+++ 3. Starting Java/HelloWorld.java
@@ -0,0 +1,23 @@
+//the public keyword here is the access modifier
+//so here public means this code can be accessed publicly
+//the class keyword is used to define a class
+public class HelloWorld {
+    //public static.... args) this is a main method
+    //method is a collection of statements that perform an operation
+    //void is used because there is no return value
+    //String[] args is a parameter
+    //Also java is case sensitive so the below code should be as it is
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+//This is the basic code which needs to be written in every code
+//public class should be written followed by the file name
+//As you can see here the file name is HelloWorld.java so
+/*
+public class YourFileName {
+    public static void main(String[] args) {
+    }
+}
+This is the basic code broiler plate you have to write before coding anything in java
+ */
