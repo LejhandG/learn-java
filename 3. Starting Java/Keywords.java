a242bbfa26eb81dbb17bc66d9584b9be70b8f367
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2023-12-14 14:55:28 +0530

    6. Keywords and Expressions

---
+++ 3. Starting Java/Keywords.java
@@ -0,0 +1,17 @@
+public class Keywords {
+    public static void main(String[] args) {
+        //Java has got 51 keywords, keywords are reserved words which have a predefined meaninng
+        //And cannot be used as variables
+        //https://docs.oracle.com/javase/specs/jls/se17/html/jls-3.html#jls-3.9
+        //int int = 5; //This is wrong as int is a keyword and cannot be used as a variable name
+        int int2 = 10; //This is correct as int2 is not a keyword
+        //In Java, an expression is a combination of variables, operators, and method invocations that evaluates to a single value
+        int hello = 5;
+        //here int is the datatype
+        //hello is the variable name
+        //= is an assignment operator
+        //5 is the data or the value
+        //and the semicolon (;)
+        //and all this combined together is an expression
+    }
+}
