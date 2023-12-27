b74974299c1593bca7bc2c85c88a17f60cad1856
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2023-12-27 09:51:05 +0530

    5. Classes And Objects

---
+++ 4. Control Flow Statements/Class.java
@@ -0,0 +1,11 @@
+public class Class { //<- this is a class
+    public static void main(String[] args) {
+        //What are classes?
+        //In easy terms class is a way of storing data
+        //It is a custom datatype which contains methods
+        //A class is like an empty form that gets copied and handed out
+        //What is an object?
+        //Object is an instance of a particular class
+        //The most common way of creating a new object is the "new" keyword
+    }
+}
