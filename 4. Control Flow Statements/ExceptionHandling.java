a1a6b586e2799a1111d42c4c309c451a69cb984f
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2023-12-28 10:14:56 +0530

    6. Handle Errors

---
+++ 4. Control Flow Statements/ExceptionHandling.java
@@ -0,0 +1,13 @@
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        /*
+        An exception is caught, first by creating a code block around the code, that might get the error.
+        This is done with the try statement code block.
+         */
+        try {
+            //statements that might get errors
+        } catch (NullPointerException e) { //this is an example. Here add the error you want to catch
+            //code to handle the exception
+        }
+    }
+}
